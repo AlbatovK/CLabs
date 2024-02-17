9477e77b008954950e0a9539483c7b6f8260ef55
Konstantin Albatov <albatovkonstantin@yandex.ru>
2024-02-17 18:56:58 +0300

    :hammer: Lab1 base

--- algorithms_data_structures/lab_1/main.c
+++ algorithms_data_structures/lab_1/main.c
@@ -1,6 +1,6 @@
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 
 typedef struct Line {
     int size;
@@ -28,7 +28,7 @@ int getInt(int *a) {
             return 0;
         }
         if (n == 0) {
-            printf("Invalid input.\n");
+            printf("Invalid input. Enter again.\n");
             scanf("%*c");
         }
     } while (n == 0);
@@ -49,54 +49,99 @@ void erase(Matrix *matrix) {
         free(matrix->lines[i].arr);
     }
     free(matrix->lines);
+    matrix->linesCount = 0;
+    matrix->lines = NULL;
 }
 
 int input(Matrix *matrix) {
-    printf("Enter lines count.\n");
     int linesCount = 0;
+    const char *pr = "";
+
+    printf("Enter lines count.\n");
+
     do {
-        getInt(&linesCount);
+        printf("%s", pr);
+
+        pr = "Lines count must be more than 0.\n";
+        if (getInt(&linesCount) == 0) {
+            return 0;
+        }
+
     } while (linesCount < 1);
 
     matrix->linesCount = linesCount;
     matrix->lines = (Line *)calloc(matrix->linesCount, sizeof(Line));
+
     for (int i = 0; i < matrix->linesCount; i++) {
-        printf("Enter line size for line %d.\n", i);
-        int lineSize = 0;
+        int lineSize;
+        pr = "";
+
+        printf("Enter line size for line %d.\n", i + 1);
+
         do {
-            getInt(&lineSize);
+            printf("%s", pr);
+
+            pr = "Lines count must be more than 0.\n";
+
+            if (getInt(&lineSize) == 0) {
+                matrix->linesCount = i;
+                erase(matrix);
+                return 0;
+            }
+
         } while (lineSize < 1);
 
         matrix->lines[i].size = lineSize;
         matrix->lines[i].arr = malloc(lineSize * sizeof(int));
 
-        printf("Enter line items for line %d.\n", i);
+        printf("Enter line items for line %d.\n", i + 1);
+
         for (int j = 0; j < lineSize; j++) {
             int item;
-            getInt(&item);
+
+            if (getInt(&item) == 0) {
+                matrix->linesCount = i + 1;
+                erase(matrix);
+                return 0;
+            }
+
             matrix->lines[i].arr[j] = item;
         }
     }
+
+    return 1;
 }
 
-Matrix *ind_task() {
+void ind_task() {
     int m;
+    const char *pr = "";
+
+    printf("Enter vector size.\n");
     do {
+        printf("%s", pr);
+        pr = "Size must be more than 0.\n";
         getInt(&m);
     } while (m < 1);
 
     int *arr = (int *)malloc(m * sizeof(int));
 
-    Matrix * matrix = (Matrix *) malloc(sizeof(Matrix));
+    Matrix *matrix = (Matrix *)malloc(sizeof(Matrix));
     matrix->linesCount = m;
-    matrix->lines = (Line *) malloc(m * sizeof(Line));
+    matrix->lines = (Line *)malloc(m * sizeof(Line));
+
+    printf("Enter elements:\n");
 
     int mx = INT_MIN;
     for (int i = 0; i < m; i++) {
-        scanf("%d", arr + i);
-        if (arr[i] > mx) {
-            mx = arr[i];
-        }
+        pr = "";
+        do {
+            printf("%s", pr);
+            pr = "Values must be greater or equal to zero.\n";
+            getInt(arr + i);
+            if (arr[i] > mx) {
+                mx = arr[i];
+            }
+        } while (arr[i] < 0);
     }
 
     int f_c = 2;
@@ -108,7 +153,7 @@ Matrix *ind_task() {
         f_c += 1;
     }
 
-    int *f_arr = (int *) malloc(f_c * sizeof(int));
+    int *f_arr = (int *)malloc(f_c * sizeof(int));
     f_arr[0] = f_arr[1] = 1;
     for (int i = 2; i < f_c; i++) {
         f_arr[i] = f_arr[i - 1] + f_arr[i - 2];
@@ -127,8 +172,8 @@ Matrix *ind_task() {
                 k += 1;
             }
         }
-        matrix -> lines[i].size = k;
-        matrix -> lines[i].arr = (int *) malloc(k * sizeof(int));
+        matrix->lines[i].size = k;
+        matrix->lines[i].arr = (int *)malloc(k * sizeof(int));
     }
 
     for (int i = 0; i < m; i++) {
@@ -140,7 +185,7 @@ Matrix *ind_task() {
                 j--;
             } else {
                 sm -= f_arr[j];
-                matrix -> lines[i].arr[k] = f_arr[j];
+                matrix->lines[i].arr[k] = f_arr[j];
                 k += 1;
             }
         }
@@ -173,6 +218,7 @@ int main() {
     // erase(matrix);
 
     // free(matrix);
+
     ind_task();
     return 0;
 }
