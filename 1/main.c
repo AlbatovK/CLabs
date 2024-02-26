b117404a565f66ca88c59a2c53bc87ebcdd0c607
albatov.ke <albatov.ke@dozen.mephi.ru>
2024-02-26 19:17:23 +0300

    :hammer: Lab1

---
+++ 1/main.c
@@ -0,0 +1,14 @@
+#include "matrix.h"
+
+int main() {
+    // Matrix *matrix = malloc(sizeof(Matrix));
+
+    // input(matrix);
+    // output(*matrix);
+    // erase(matrix);
+
+    // free(matrix);
+
+    ind_task();
+    return 0;
+}
