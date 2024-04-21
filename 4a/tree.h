2d05dfb8fcca1973817784c2cb0d54c2b95db923
albatov.ke <albatov.ke@dozen.mephi.ru>
2024-04-21 22:22:55 +0300

    First Stable

---
+++ 4a/tree.h
@@ -0,0 +1,48 @@
+#ifndef TREE_H
+#define TREE_H
+
+typedef struct InfoField {
+
+	int value;
+
+	struct InfoField * next;
+	
+} InfoField;
+
+typedef struct TreeNode {
+
+	InfoField * infoNode;
+	
+	int key;
+
+	struct TreeNode * prev;
+
+	struct TreeNode * left;
+
+	struct TreeNode * right;
+	
+} TreeNode;
+
+void insert(TreeNode * * node, int key, int value, TreeNode * prev);
+
+TreeNode * delete(TreeNode * * node, int key);
+
+TreeNode * importFromFile(char * filename);
+
+TreeNode * findNearestByKey(TreeNode * root, int key);
+
+TreeNode * search(TreeNode * node, int key);
+
+void freeTree(TreeNode * node);
+
+TreeNode * processFile(char * filename, char * output);
+
+void specialSearch(TreeNode * node, int digits);
+
+void dot_print_tree(TreeNode * root, char * filename);
+
+void std_print_tree(TreeNode * root, int offset);
+
+InfoField * searchByKeyAndPos(TreeNode * node, int key, int pos);
+
+#endif
